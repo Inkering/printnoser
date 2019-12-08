a0fdf8031f38b62528247b246ae19a0628af3737
Sam Daitzman <sam@daitzman.com>
2019-12-08 15:52:58 -0500

    reskins site for QEA

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -18,6 +18,7 @@
 
 <script>
 import Vue from 'vue'
+import $ from 'jquery'
 // internal components
 import Nav from "@theme/components/Nav";
 import Footer from "@theme/components/Footer";
@@ -41,10 +42,36 @@ export default {
 				}
 		},
 };
+
+$(document).ready(function () {
+            // Add smooth scrolling to all links
+            $("a").on('click', function (event) {
+
+                // Make sure this.hash has a value before overriding default behavior
+                if (this.hash !== "") {
+                    // Prevent default anchor click behavior
+                    event.preventDefault();
+
+                    // Store hash
+                    var hash = this.hash;
+
+                    // Using jQuery's animate() method to add smooth page scroll
+                    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 250, function () {
+
+                        // Add hash (#) to URL when done scrolling (default click behavior)
+                        window.location.hash = hash;
+                    });
+                } // End if
+            });
+        });
+
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911|Lato&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Gelasio|Poppins:700&display=swap');
 @import '../styles/theme.css';
 </style>
 
