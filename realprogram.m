864e8cf6c4897b409475c8aac8cef983a4ff0182
DIETER BREHM <inkering@gmail.com>
2019-12-09 12:25:21 -0500

    outline program functionality

---
+++ realprogram.m
@@ -0,0 +1,11 @@
+% 3D printing analysis script
+% determines whether a print is likely to have succeeded based on
+% accelerometer data of a phone placed on the print head.
+
+% function for parsing acelerometer data into sufficient matrix
+
+% function for performing fft, generating plots for logging, and outputting
+% fft data
+
+% function for classification of data as fail/not fail, optionally with a
+% confidence rating
