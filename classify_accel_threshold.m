837a8711f6637d4776e4da8c80af5240f223c353
Sam Daitzman <sam@daitzman.com>
2019-12-09 18:50:43 -0500

    working accel threshold failure analysis

---
+++ classify_accel_threshold.m
@@ -0,0 +1,18 @@
+function [success, level] = classify_accel_threshold(data, threshold)
+% Uses a basic outlier-reducing threshold to diagnose print fails
+
+if nargin < 2 % if not passed a threshold
+    threshold = 15; % set default threshold
+end
+
+largest = maxk(data, 3) % get 10 largest FFT output values
+meanHighAccel = mean(largest); % take their mean
+level = meanHighAccel;
+
+if meanHighAccel > threshold
+    success = false
+else
+    success = true
+end
+
+end
