77c992b0477e05114548056eaf871282c170f1c0
Sam Daitzman <sam@daitzman.com>
2019-12-09 23:15:12 -0500

    imports and plots all data

--- analyze_all.m
+++ analyze_all.m
@@ -7,10 +7,11 @@ mydata = cell(1, numfiles);
 % Import Data
 for k = 1:numfiles
   mydata{k} = importdata(strcat('data/',datasets(k).name));
+  mydata{k}.Source = datasets(k).name;
 end
 
 % Run simple analysis on all
 for k = 1:length(mydata)
     sample = mydata(k);
-    sample{1}.Acceleration
+    sample{1}.Acceleration;
 end
