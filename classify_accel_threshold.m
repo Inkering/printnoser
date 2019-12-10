3f2367f2fdd1461580ae5ec70611820ddd0aff02
Sam Daitzman <sam@daitzman.com>
2019-12-10 01:06:50 -0500

    more analysis

--- classify_accel_threshold.m
+++ classify_accel_threshold.m
@@ -5,14 +5,14 @@ if nargin < 2 % if not passed a threshold
     threshold = 15; % set default threshold
 end
 
-largest = maxk(data, 3) % get 10 largest FFT output values
+largest = maxk(data, 3); % get 10 largest FFT output values
 meanHighAccel = mean(largest); % take their mean
 level = meanHighAccel;
 
 if meanHighAccel > threshold
-    success = false
+    success = false;
 else
-    success = true
+    success = true;
 end
 
 end
