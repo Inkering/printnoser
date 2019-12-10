e56fda1cb800ea50e00650a05118064f6694e920
Sam Daitzman <sam@daitzman.com>
2019-12-10 03:33:30 -0500

    more detailed analysis

--- classify_fft_threshold.m
+++ classify_fft_threshold.m
@@ -1,21 +1,18 @@
 function [success, level] = classify_fft_threshold(data, threshold)
 % Uses a basic outlier-reducing threshold to diagnose print fails
 
-d = round(length(data)/6);
-data = data(d:end-d);
-
 if nargin < 2 % if not passed a threshold
     threshold = 0.02; % set default threshold
 end
 
-largest = maxk(data, 3) % get 10 largest FFT output values
+largest = maxk(data, 3); % get 10 largest FFT output values
 meanHighPower = mean(largest); % take their mean
 level = meanHighPower;
 
 if meanHighPower > threshold
-    success = false
+    success = false;
 else
-    success = true
+    success = true;
 end
 
 end
